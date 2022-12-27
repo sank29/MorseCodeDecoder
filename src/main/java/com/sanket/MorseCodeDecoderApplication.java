df3ddb91e743a604410ae83addea287d090d640f
sanket <wankhdes29@gmail.com>
2022-12-27 22:55:21 +0530

    spring starter create

---
+++ src/main/java/com/sanket/MorseCodeDecoderApplication.java
@@ -0,0 +1,13 @@
+package com.sanket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorseCodeDecoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorseCodeDecoderApplication.class, args);
+	}
+
+}
